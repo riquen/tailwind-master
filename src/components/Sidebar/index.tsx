866dc827b25eda8cda908860e57d8d2d5fbada16
Henrique Nunes <henri.arantes1@gmail.com>
2024-03-04 17:28:36 -0300

    feat: init responsive

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -17,7 +17,7 @@ import { InputControl, InputPrefix, InputRoot } from '../Input'
 
 export const Sidebar = () => {
   return (
-    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="fixed bottom-0 left-0 right-0 top-0 z-20 flex h-screen flex-col gap-6 border-b border-zinc-200 bg-white p-4 lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8">
       <Logo />
       <InputRoot>
         <InputPrefix>
